f8d87c9a3fa407f79fd3141bab1b611a25eb2595
Oliver Dressler <oliver.dressler@chem.ethz.ch>
2013-12-05 13:49:59 +0100

    Improved slightly

--- InspirationEngine.py
+++ InspirationEngine.py
@@ -1,25 +1,32 @@
 import pygame
 from resources import *
 from geometry import *
-import engine
+from engine import BaseCanvas
 import time
 
 # Init pygame
 pygame.init()
 
+class InspirationEngine(BaseCanvas):
+	def __init__(self, size = (1000, 800), caption = 'Inspiration Engine 0.1', max_fps = 40):
+		self.size = size
+		BaseCanvas.__init__(self, size = size, caption = caption, max_fps = max_fps)
+
+	def repeat_func(self):
+		if not (self.tick+1)%50:
+			random_query = randomSentence()
+			print random_query
+			random_image = googleImage(random_query)
+			random_point = randomPoint(self.size)
+			self.add_image(googleImage(randomSentence()), random_point, size_x = random.randrange(100, 250))
+			
+		return True
+
 def main():
-	canvas = engine.Canvas()
-
-	# Add an image
-	img1 = googleImage('Bread Castle')
-	img2 = googleImage('Sun Blue')
-
+	canvas = InspirationEngine()
 
 	# Start canvas animation
 	canvas.start()
-
-	canvas.add_image(img1, Point(500, 400), size_x = 500, moving = Point(5, 5))
-	canvas.add_image(img2, Point(250, 250), size_x = 300)
 
 if __name__ == '__main__':
 	main()
